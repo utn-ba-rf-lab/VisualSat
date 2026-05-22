4baacc613db54208015bc7585ae9411ab6825b95
Javier Maita <javiermaita@hotmail.com.ar>
2026-05-22 10:21:24 -0300

    integracion de Endpoint proveedor de imagenes

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-images_raw_path = "/home/javiermaita/Documentos/PID/processed-images"
+#Ruta de imagenes procesadas (JAVIER - CAMBIAR PARA RFLAB)
+images_raw_path = "/home/javiermaita/Imágenes/prueba"
+images = os.listdir(images_raw_path)
 
 # Montar la carpeta "static" para servir archivos como imágenes, CSS, JS
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -17,7 +19,30 @@ app.mount("/images-raw", StaticFiles(directory=images_raw_path), name="images-ra
 # Motor de plantillas
 templates = Jinja2Templates(directory="templates")
 
+#API
+#---Home
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    images = os.listdir(images_raw_path)
-    return templates.TemplateResponse("home.html", {"request": request, "images": images})
+
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={"images": images}
+    )
+
+#---Servir imagenes
+@app.get("/imagenes")
+async def obtener_imagenes(request: Request):
+
+    imagenes = []
+
+    for archivo in images:
+        if archivo.lower().endswith(".jpeg"):
+            imagenes.append({
+                "nombre": archivo,
+                "url": str(request.base_url) + f"images-raw/{archivo}"
+            })
+
+    return {
+        "imagenes": imagenes
+    }
